964f517f9e6f2c5cfedc261e14f8b5b8205323f3
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-05-15 15:05:27 -0600

    Create number guessing functionality

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// Create random number variable between 1 and 20
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+// Assign the random number to the number box
+document.querySelector('.number').textContent = secretNumber;
+
+// When the check button is clicked, get the guess from the input field and compare it against the secret number
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number!';
+
+    // Make the background color green and the number box wider
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
